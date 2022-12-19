9f640764949a84979f94574b60569fab3e1f3bbe
Ryota <aya.ryo.718@outlook.jp>
2022-12-19 21:14:39 +0900

    google mapの埋め込みの追加

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function NewPlace() {
   return (
-    <div>NewPlace works!</div>
-  )
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d223740.93487102084!2d139.48400100243563!3d35.90719671092136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d1a4869b805%3A0x11e214c5de0808a8!2z5pep56iy55Sw5aSn5a2m!5e0!3m2!1sja!2sjp!4v1671452020914!5m2!1sja!2sjp"
+      width="600"
+      height="450"
+      style="border:0;"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
 }
 
-export default NewPlace
+export default NewPlace;
